677843d87d4ecd63e2608b9c0f47a38e56a8eada
Shubham Kumar Singh <102406929+ShubhamGet@users.noreply.github.com>
2022-04-10 06:26:14 -0700

    Add files via upload

---
+++ arithematic.dart
@@ -0,0 +1,25 @@
+void main() {
+  int num1 = 10;
+  var num2 = 10;
+  var res = 0;
+  res = num1 + num2;
+  print("Addition: ${res}");
+
+  res = num1 - num2;
+  print("Subtraction: ${res}");
+
+  res = num1 * num2;
+  print("Multiplication: ${res}");
+
+  res = num1 - num2;
+  print("Division: ${res}");
+
+  res = num1 % num2;
+  print("MODULS: ${res}");
+
+  num1++;
+  print("Increment : $num1");
+
+  num1--;
+  print("Decrement : $num1");
+}
